48b49013173b468de5cd24ed6515e447da603074
egoTr <luongvannghia@gmail.com>
2021-07-09 20:34:00 +0700

    2021 July, 09#2

---
+++ nextjs-course/pages/[productId].js
@@ -0,0 +1,40 @@
+import data from '../dummy-backend.json';
+
+export default function ProductDetails({ product }) {
+    if (!product)
+        return <p>Loading...</p>;
+
+    return <>
+        <h1>{product.id}</h1>
+        <p>{product.title}</p>
+    </>
+}
+
+export async function getStaticPaths() {
+    const products = data.products;
+
+    const paths = products.map(p => {
+        return { params: { productId: p.id } }
+    })
+
+    return {
+        paths,  // could be partial listed
+        fallback: false
+        // false: show 404 page if not listed
+        // true: show fallback content (implemented in the component function) while generating
+        // 'blocking': block page while generating
+    }
+}
+
+export async function getStaticProps(context) {
+    const { params } = context;
+    const productId = params.productId;
+
+    const product = data.products.find(p => p.id === productId);
+
+    return {
+        props: {
+            product
+        }
+    }
+}
--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -1,10 +1,67 @@
+// dependences
+import fs from 'fs';
 import Head from 'next/head';
+import Link from 'next/link';
 
-export default function Home() {
+// data
+import data from '../dummy-backend.json';
+
+export default function Home(props) {
+  const products = data['products'];
+
   return <>
     <Head>
       <title>Home - NextJS Course</title>
     </Head>
-    <h1>Home</h1>
+
+    <h1>Products</h1>
+    <i>(Data populated at client-side)</i>
+    <ul>
+      {products.map(product =>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      )}
+    </ul>
+
+    <hr />
+
+    <h1>Items inside of current folder</h1>
+    <i>(Data populated at server-side)</i>
+    {props.lists.map((item, i) => <li key={i}><code>{item.name}</code></li>)}
   </>
+}
+
+export async function getStaticProps(context) {
+  const dirToList = process.cwd();
+  const files = await fs.readdirSync(dirToList);
+  const filesObject = new Array();
+  for (let i = 0; i < files.length; i++) {
+    let file = {
+      dirId: i + 1,
+      name: files[i]
+    };
+    filesObject.push(file)
+  }
+
+  if (!filesObject)
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  
+    if (filesObject.length === 0)
+    return {
+      notFound: true
+    }
+
+  return {
+    props: {
+      lists: filesObject
+    },
+    revalidate: 86_400, // re-generate every 24 hours
+    // redirect,
+    // notFound
+  }
 }
